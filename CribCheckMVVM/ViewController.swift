094f859b46519dbc300a401b2fdd088816b64284
Lisa <imnotlostrui@aim.com>
2018-11-06 22:57:55 -0500

    Initial Commit

---
+++ CribCheckMVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CribCheckMVVM
+//
+//  Created by Lisa J on 11/6/18.
+//  Copyright © 2018 Lisa J. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
